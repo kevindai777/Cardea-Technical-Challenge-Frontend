c5da99fb8bf09f309fcdc44a2a69642cfd7c98c6
Kevin Dai <kevin.dai.2451@gmail.com>
2020-09-06 23:30:07 -0400

    technical requirements done

---
+++ src/components/Category.tsx
@@ -0,0 +1,46 @@
+import React from "react"
+import gql from "graphql-tag"
+import { useQuery } from "@apollo/react-hooks"
+import { HelloWorldQuery } from "./__generated__/HelloWorldQuery";
+
+interface ICategoryProps {
+    name?: string[];
+    id?: number;
+    set(id: any): any;
+    setAgain(id: any): any;
+}
+
+interface ICategoryState {
+    reveal: boolean;
+}
+
+class Category extends React.Component<ICategoryProps, ICategoryState> {
+    state = {
+        reveal: false
+    }
+
+    changeReveal = () => {
+        this.setState({
+            reveal: !this.state.reveal
+        })
+    }
+
+    createButton = () => {
+        return this.state.reveal ? <button onClick={(event) => {this.changeReveal(); this.props.setAgain(this.props.id)}}>Hide</button> : <button onClick={(event) => {this.changeReveal(); this.props.set(this.props.id)}}>Reveal</button>
+    }
+
+    determineIfHere = () => {
+        return this.state.reveal ? this.props.name : null
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.name && <h1>{this.props.name}</h1>}
+                {this.props.id ? this.createButton() : null}
+            </div>
+        )
+    }
+}
+
+export default Category
--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,22 +1,77 @@
-import React from "react"
-import gql from "graphql-tag"
-import { useQuery } from "@apollo/react-hooks"
-import {HelloWorldQuery} from "./__generated__/HelloWorldQuery";
+import React, { useState } from "react";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+import { HelloWorldQuery } from "./__generated__/HelloWorldQuery";
+import Category from './Category';
+import Job from './Job';
 
 const HELLOWORLD_QUERY = gql`
     query HelloWorldQuery {
-        hello
+        categories {
+            id
+            name
+        }
+        jobcategories {
+            id
+            jobID
+            categoryID
+        }
+        jobs {
+            id
+            title
+        }
     }
 `
 
 const HelloWorld = () => {
-    const { loading, error, data } = useQuery<HelloWorldQuery>(HELLOWORLD_QUERY, {
-    })
+    let [arrayOfCategories, setArray] = useState([]) as any;
+    
+    const { loading, error, data } = useQuery<HelloWorldQuery>(HELLOWORLD_QUERY, {})
+
+    let set = function(value: string) {
+        if (!arrayOfCategories.includes(value)) {   
+            setArray([...arrayOfCategories, value])
+        }
+    }
+
+    let setAgain = function(value: string) {
+        setArray(arrayOfCategories.filter((string: any) => string != value))
+    }
+
+    let mapTitles = function(datalog: any) {
+        let toUse = datalog.categories
+    
+        return toUse.map((object: any) => 
+            <Category 
+                name={object.name}
+                id={object.id}
+                set={set}
+                setAgain={setAgain}
+            />
+        )
+    }
+    
+    let mapJobs = function(datalogs: any) {
+        let toUse = datalogs.jobcategories 
+    
+        return toUse.map((object: any) =>
+            <Job
+                jobID={object.jobID}
+                categoryID={object.categoryID}
+                categories={arrayOfCategories}
+            />
+        )    
+    }
+
     return(
         <>
             {loading && <h1>Loading</h1>}
             {error && <h1>Error</h1>}
-            {data && <h1>{data.hello}</h1>}
+            {data ? mapTitles(data) : null}
+            <div>
+                <h1>Jobs:</h1>
+            {data ? mapJobs(data) : null}
+            </div>
         </>
     )
 }
